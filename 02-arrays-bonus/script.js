71661ad867e83e895c2a0fe2824631aafe02e155
Marco-Petronella <marco.petronella85@gmail.com>
2026-03-04 12:32:48 +0100

    array bonus 1

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -8,9 +8,14 @@ const teachers = [
   'Luca'
 ]; // NON MODIFICARE QUESTA VARIABILE
 
+// stampa di debug
+console.log(teachers);
+
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = [];
+for (let i=0; i<teachers.length; i++)
+  reversedTeachers.unshift(teachers[i]);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
@@ -24,3 +29,6 @@ const isFabioPresent = null;
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
+
+// stampa di debug
+console.log(reversedTeachers);
